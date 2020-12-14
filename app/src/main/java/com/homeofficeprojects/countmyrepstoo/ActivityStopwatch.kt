b274a0ad0fb219fd3d1c469fe7097d3876547806
djadu <dvorkin.vitaliy@gmail.com>
2020-12-13 21:29:13 -0500

    New Laptop Commit

--- app/src/main/java/com/homeofficeprojects/countmyrepstoo/ActivityStopwatch.kt
+++ app/src/main/java/com/homeofficeprojects/countmyrepstoo/ActivityStopwatch.kt
@@ -2,10 +2,17 @@ package com.homeofficeprojects.countmyrepstoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Fade
 
 class ActivityStopwatch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stopwatch)
+
+        title = "Activity Stopwatch"
+
+        val fade = Fade()
+        window.enterTransition = fade
+        window.exitTransition = fade
     }
 }
--- app/src/main/java/com/homeofficeprojects/countmyrepstoo/StopwatchFragment.kt
+++ app/src/main/java/com/homeofficeprojects/countmyrepstoo/StopwatchFragment.kt
@@ -1,19 +1,62 @@
 package com.homeofficeprojects.countmyrepstoo
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.transition.Fade
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
+import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import androidx.core.view.ViewCompat
+import kotlinx.android.synthetic.main.fragment_stopwatch.view.*
 
 
 class StopwatchFragment : Fragment() {
+
+    lateinit var minutesTextView: TextView
+    lateinit var secondsTextView: TextView
+    lateinit var millisTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stopwatch, container, false)
+        val view = inflater.inflate(R.layout.fragment_stopwatch, container, false)
+
+        val fade = Fade()
+
+        activity?.title = "Fragment Stopwatch"
+
+
+        minutesTextView = view.textView_stopwatch_minutes_fragment
+        secondsTextView = view.textView_stopwatch_seconds_fragment
+        millisTextView = view.textView_stopwatch_millis_fragment
+        view.button_start_stopwatch_fragment.setOnClickListener {
+            startStopwatchActivity()
+        }
+
+        activity?.window?.enterTransition = fade
+        activity?.window?.exitTransition = fade
+        return view
+    }
+
+    private fun startStopwatchActivity() {
+        val intent = Intent(activity, ActivityStopwatch::class.java)
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+            activity!!,
+            Pair.create(minutesTextView, minutesTextView.transitionName),
+            Pair.create(secondsTextView, secondsTextView.transitionName),
+            Pair.create(millisTextView, millisTextView.transitionName)
+
+
+        )
+
+        startActivity(intent, options.toBundle())
     }
 }
